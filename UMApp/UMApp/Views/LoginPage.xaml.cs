39d7c3371624e9d96b508a905e074c49d4f892ca
zero <zero.xiong@gmail.com>
2018-05-04 23:32:16 +0800

    mobile

---
+++ UMApp/UMApp/Views/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UMApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Test", "I am testing", "OK");
+        }
+    }
+}
---
+++ UMDomainEntity/Client.cs
@@ -0,0 +1,6 @@
+﻿namespace UMDomainEntity
+{
+    public class Client
+    {
+    }
+}
